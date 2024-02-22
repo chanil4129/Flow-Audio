bcf63796918983fa27595c125a70288b555aa692
Kim Chan Il <43468017+chanil4129@users.noreply.github.com>
2024-02-22 19:09:37 +0900

    feat: 기능 추가
    현재 재생 UI 추가
    5초 되돌리기 추가
    재생/일시정지 버튼 추가
    현재 재생중인 unit 또 누르면 해당 unit 처음부터 재생 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,12 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:http/http.dart' as http;
 
 void main() async {
-  print("APP START");
-  try {
-    await dotenv.load(fileName: "setting.env");
-    print("ENV LOADED");
-  } catch (e) {
-    print("Failed to load .env file: $e");
-  }
+  await dotenv.load(fileName: "setting.env");
   runApp(MyApp());
 }
 
@@ -20,12 +17,17 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   final AudioPlayer _audioPlayer = AudioPlayer();
-  int currentUnit = 1;
+  int currentUnit = 0;
   final int totalUnits = 200;
 
   @override
   void initState() {
     super.initState();
+    _audioPlayer.onPlayerStateChanged.listen((state) {
+      // AudioPlayer 상태가 변할 때마다 UI를 갱신
+      setState(() {});
+    });
+
     _audioPlayer.onPlayerComplete.listen((event) {
       // 재생이 완료된 후 다음 UNIT 재생
       if (currentUnit < totalUnits) {
@@ -37,15 +39,49 @@ class _MyAppState extends State<MyApp> {
     });
   }
 
+  Future<String> _getFilePath(int unit) async {
+    final directory = await getApplicationDocumentsDirectory();
+    final filePath = '${directory.path}/$unit.mp3';
+    return filePath;
+  }
+
+  Future<void> _cacheOrPlayUnit(int unit) async {
+    final filePath = await _getFilePath(unit);
+    final file = File(filePath);
+
+    if (await file.exists()) {
+      print('cache UNIT $unit');
+      _audioPlayer.play(DeviceFileSource(filePath));
+    } else {
+      final url = '${dotenv.env['AUDIO_URL']}$unit.mp3';
+      final response = await http.get(Uri.parse(url));
+      if (response.statusCode == 200) {
+        await file.writeAsBytes(response.bodyBytes);
+        print('playing UNIT $unit');
+        _audioPlayer.play(DeviceFileSource(filePath));
+      }
+    }
+  }
+
   void _playUnit(int unit) {
-    final url = '${dotenv.env['AUDIO_URL']}$unit.mp3';
-    _audioPlayer.play(UrlSource(url)); // URL을 재생
-    print('Playing UNIT $unit');
+    _cacheOrPlayUnit(unit);
+  }
+
+  void _rewind5Seconds() async {
+    Duration? currentPosition = await _audioPlayer.getCurrentPosition();
+    if (currentPosition != null) { // currentPosition이 null이 아닐 때만 실행
+      Duration rewindPosition = currentPosition - Duration(seconds: 5);
+      if (rewindPosition < Duration.zero) {
+        rewindPosition = Duration.zero;
+      }
+      _audioPlayer.seek(rewindPosition);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           title: Text('UNIT Player'),
@@ -58,12 +94,44 @@ class _MyAppState extends State<MyApp> {
               title: Text('UNIT $unitNumber'),
               onTap: () {
                 setState(() {
-                  currentUnit = unitNumber; // 선택된 UNIT으로 현재 UNIT 설정
-                  _playUnit(currentUnit); // 선택된 UNIT 재생
+                  if (currentUnit == unitNumber) {
+                    // 이미 실행 중인 유닛을 다시 눌렀을 경우, 시작 지점으로 이동
+                    _audioPlayer.seek(Duration.zero);
+                    _audioPlayer.resume(); // 시작 지점에서 재생을 재개
+                  } else {
+                    currentUnit = unitNumber; // 선택된 UNIT으로 현재 UNIT 설정
+                    _playUnit(currentUnit); // 선택된 UNIT 재생
+                  }
                 });
               },
             );
           },
+        ),
+        bottomNavigationBar: BottomAppBar(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Text('Unit $currentUnit'),
+              IconButton(
+                icon: Icon(Icons.replay_5),
+                onPressed: _rewind5Seconds,
+              ),
+              IconButton( // Play/Pause 토글 버튼
+                icon: Icon(_audioPlayer.state == PlayerState.playing
+                    ? Icons.pause
+                    : Icons.play_arrow),
+                onPressed: () {
+                  if (_audioPlayer.state == PlayerState.playing) {
+                    _audioPlayer.pause();
+                  } else if (_audioPlayer.state == PlayerState.paused) {
+                    _audioPlayer.resume();
+                  } else {
+                    _playUnit(currentUnit);
+                  }
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
